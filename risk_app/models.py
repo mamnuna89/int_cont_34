a99c67bec05fa8eec3c2b39219a13f6c8a4cb10b
mamnuna89 <mamnunakhon.mahmudova@alif.tj>
2025-05-21 17:41:42 +0500

    Create models.py

---
+++ risk_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Risk(models.Model):
+    number = models.AutoField(primary_key=True)
+    description = models.TextField()
+    risk_type = models.CharField(max_length=50)
+    risk_owner = models.CharField(max_length=100)
+    process = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    likelihood = models.IntegerField()
+    impact = models.IntegerField()
+    level = models.IntegerField(editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.level = self.likelihood * self.impact
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Risk #{self.number}"
