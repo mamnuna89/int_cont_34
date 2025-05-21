3669ab00e2a4436c932d4c01d664496b0eb1f776
mamnuna89 <mamnunakhon.mahmudova@alif.tj>
2025-05-21 21:16:05 +0500

    Update urls.py

--- risk_project/urls.py
+++ risk_project/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
 from rest_framework.routers import DefaultRouter
 from risk_app.views import RiskViewSet
 
@@ -9,4 +10,5 @@ router.register(r'risks', RiskViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('', lambda request: render(request, 'index.html')),
 ]
