017c9f208f223048348a5c6a32830f888c191c76
mamnuna89 <mamnunakhon.mahmudova@alif.tj>
2025-05-22 21:46:55 +0500

    Update views.py

--- risk_app/views.py
+++ risk_app/views.py
@@ -1,7 +1,20 @@
-from rest_framework import viewsets
+from django.shortcuts import render, redirect
 from .models import Risk
-from .serializers import RiskSerializer
+from .forms import RiskForm
 
-class RiskViewSet(viewsets.ModelViewSet):
-    queryset = Risk.objects.all()
-    serializer_class = RiskSerializer
+def index(request):
+    return render(request, 'index.html')
+
+def risk_list(request):
+    risks = Risk.objects.all()
+    return render(request, 'risk_list.html', {'risks': risks})
+
+def risk_create(request):
+    if request.method == 'POST':
+        form = RiskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('risk_list')
+    else:
+        form = RiskForm()
+    return render(request, 'risk_form.html', {'form': form})
